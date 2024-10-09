18895bed6aa09a17b8c83e10ff93a4c563ab0c41
volkov.al <volkov.al@tecomgroup.ru>
2024-10-09 15:54:12 +0300

    Scaled font size current Widget and update width size widget

--- src/diff_widget/widget/abc_file_widget.py
+++ src/diff_widget/widget/abc_file_widget.py
@@ -1,16 +1,37 @@
 from PySide6.QtWidgets import QWidget, QTextEdit, QHBoxLayout
-from PySide6.QtGui import QTextOption
+from PySide6.QtGui import QTextOption, QTextCursor
+
+
+class ABCTextEdit(QTextEdit):
+    def scaled_font_size(self, new_font_size: int) -> None:
+        """Scaled font size current Widget and update width size widget
+        :param new_font_size: new size
+        """
+        cursor = self.textCursor()
+        cursor.select(QTextCursor.Document)
+        char_format = cursor.charFormat()
+        char_format.setFontPointSize(new_font_size)
+        cursor.mergeCharFormat(char_format)
+        cursor.clearSelection()
+
+
+class LineTextEdit(ABCTextEdit):
+    def scaled_font_size(self, new_font_size: int) -> None:
+        """Scaled font size current Widget and update width size widget
+        :param new_font_size: new size
+        """ 
+        super().scaled_font_size(new_font_size)
+        self.setFixedWidth(new_font_size * 4)
 
 
 class ABCFile(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-
-        self.text_edit = QTextEdit()
+        self.text_edit = ABCTextEdit()
         self.text_edit.setWordWrapMode(QTextOption.NoWrap)
 
-        self.line = QTextEdit()
+        self.line = LineTextEdit()
         self.line.setFixedWidth(50)
 
         self.layout = QHBoxLayout(self)
@@ -18,3 +39,10 @@ class ABCFile(QWidget):
 
     def draw(self):
         pass
+
+    def scaled_font_size(self, new_font_size: int) -> None:
+        """Scaled font size current Widget and update width size widget
+        :param new_font_size: new size
+        """
+        self.text_edit.scaled_font_size(new_font_size)
+        self.line.scaled_font_size(new_font_size)
--- src/diff_widget/widget/general_diff_widget.py
+++ src/diff_widget/widget/general_diff_widget.py
@@ -11,9 +11,12 @@ from .modified_file_widget import ModifiedFile
 class DiffWidget(QWidget):
     def __init__(self, files, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.current_font_size = 10
 
         self.current_file = CurrentFile()
+        self.current_file.scaled_font_size(self.current_font_size)
         self.modified_file = ModifiedFile()
+        self.modified_file.scaled_font_size(self.current_font_size)
 
         splitter = QSplitter(Qt.Horizontal)
         splitter.addWidget(self.current_file)
@@ -37,6 +40,11 @@ class DiffWidget(QWidget):
             )
 
         self.set_logical_vertical_scroll_bar()
+
+    def wheelEvent(self, event):
+        if event.modifiers() == Qt.ControlModifier:
+            self.scaled_font_size(y_mouse_rotation=event.angleDelta().y())
+            event.accept()
 
     def equals(self, index1: int, index2: int, text: str):
         text = text.replace('\n', '')
@@ -92,3 +100,17 @@ class DiffWidget(QWidget):
             self.current_file.text_edit.verticalScrollBar().setValue)
         self.modified_file.line.verticalScrollBar().valueChanged.connect(
             self.modified_file.text_edit.verticalScrollBar().setValue)
+
+    def scaled_font_size(self, y_mouse_rotation: int) -> None:
+        """Scaled font size all diff widget
+        :param y_mouse_rotation: Mouse rotation angle by z
+        """
+        if y_mouse_rotation > 0:
+            self.current_font_size += 1
+        else:
+            if self.current_font_size < 10:
+                return
+            self.current_font_size -= 1
+
+        self.current_file.scaled_font_size(self.current_font_size)
+        self.modified_file.scaled_font_size(self.current_font_size)
