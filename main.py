8e1340c1f8529763ad76dac461574dc8e2727bb8
volkov.al <volkov.al@tecomgroup.ru>
2024-10-09 10:37:21 +0300

    show text file

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+from collections import namedtuple
+
 from PySide6.QtWidgets import QApplication
 
 from src.diff_widget import widget
 
 
+Files = namedtuple('Files', ['current_file', 'modified_file'])
+
+
 def main():
+
+    files = Files(
+        current_file=r'files/file1.py',
+        modified_file=r'files/file2.py'
+    )
+
     app = QApplication([])
-    window = widget.Diff()
+    window = widget.Diff(files)
     window.setGeometry(800, 100, 800, 400)
     window.show()
     app.exec()
--- src/diff_widget/widget/general_diff_widget.py
+++ src/diff_widget/widget/general_diff_widget.py
@@ -1,14 +1,20 @@
 from PySide6.QtWidgets import (
     QWidget, QVBoxLayout, QSplitter, QTextEdit, QHBoxLayout
 )
+from PySide6.QtGui import QTextOption
 from PySide6.QtCore import Qt
 
 
 class ABCFile(QWidget):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, file, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.text = QTextEdit()
+        self.text: str = file.read()
+
+        self.text_edit = QTextEdit()
+        self.text_edit.append(self.text)
+        self.text_edit.setWordWrapMode(QTextOption.NoWrap)
+
         self.line = QTextEdit()
         self.line.setFixedWidth(50)
 
@@ -21,22 +27,26 @@ class ABCFile(QWidget):
 
 class CurrentFile(ABCFile):
     def draw(self):
-        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.text_edit)
         self.layout.addWidget(self.line)
 
 
 class ModifiedFile(ABCFile):
     def draw(self):
         self.layout.addWidget(self.line)
-        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.text_edit)
 
 
 class DiffWidget(QWidget):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, files, *args, **kwargs):  # fixme: typing
         super().__init__(*args, **kwargs)
 
-        self.current_file = CurrentFile()
-        self.modified_file = ModifiedFile()
+        with (
+                open(files.current_file, encoding='utf-8') as current_file,
+                open(files.modified_file, encoding='utf-8') as modified_file,
+        ):
+            self.current_file = CurrentFile(current_file)
+            self.modified_file = ModifiedFile(modified_file)
 
         splitter = QSplitter(Qt.Horizontal)
         splitter.addWidget(self.current_file)
