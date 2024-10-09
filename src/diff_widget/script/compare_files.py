0ac37f5768a0fd4824cb78fd7c465ec086f200a4
volkov.al <volkov.al@tecomgroup.ru>
2024-10-09 15:13:53 +0300

    show compare files in general diff widget

--- src/diff_widget/script/compare_files.py
+++ src/diff_widget/script/compare_files.py
@@ -1,7 +1,15 @@
 from difflib import SequenceMatcher
 
 
-def compare_files(lines1, lines2, sequence_percent: int = 80):
+def compare_files(
+        lines1,
+        lines2,
+        func_equals,
+        func_modified,
+        func_remove,
+        func_added,
+        sequence_percent: int = 80,
+):
     """Сравнивает два файла и выводит различия в формате git diff."""
     index1 = index2 = 0
 
@@ -9,31 +17,30 @@ def compare_files(lines1, lines2, sequence_percent: int = 80):
         try:
             line1 = lines1[index1]
         except IndexError:
-            if index2 <= len(lines2):
+            if index2 >= len(lines2):
                 break
 
-            line2 = lines2[index2]
-            print(index2 + 1, '-', '-', line2, end='')
+            func_added(index2 + 1, lines2[index2])
             index2 = index2 + 1
             continue
 
         try:
             line2 = lines2[index2]
         except IndexError:
-            print(index1 + 1, '-', '-', line1, end='')
+            func_remove(index1 + 1, line1)
             if index1 + 1 != len(lines1):
                 index1 = index1 + 1
                 continue
             break
 
         if line1 == line2:
-            print(index1 + 1, index2 + 1, line1, end='')
+            func_equals(index1, index2, line1)
             index1, index2 = index1 + 1, index2 + 1
             continue
 
         percentage_match = SequenceMatcher(None, line1, line2).ratio() * 100
         if percentage_match > sequence_percent:
-            print(index1 + 1, index2 + 1, '+/-', line1.replace('\n', ''), line2.replace('\n', ''))
+            func_modified(index1, index2, line1, line2)
             index1, index2 = index1 + 1, index2 + 1
             continue
 
@@ -45,9 +52,9 @@ def compare_files(lines1, lines2, sequence_percent: int = 80):
                         percentage_match = SequenceMatcher(None, lines1[index1 + ii1], lines2[index2 + ii2]).ratio() * 100
                         if percentage_match > sequence_percent:
                             for iii2 in range(ii2):
-                                print('-', index2 + iii2, '+', lines2[index2 + iii2].replace('\n', ''))
+                                func_added(index2 + iii2, lines2[index2 + iii2])
                             for iii1 in range(ii1):
-                                print(index1 + iii1, '-', '-', lines1[index1 + iii1].replace('\n', ''))
+                                func_remove(index1 + iii1, lines1[index1 + iii1])
                             index1, index2 = index1 + ii1, index2 + ii2
                             raise StopIteration
 
--- src/diff_widget/widget/general_diff_widget.py
+++ src/diff_widget/widget/general_diff_widget.py
@@ -43,16 +43,6 @@ class DiffWidget(QWidget):
     def __init__(self, files, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        with (
-                open(files.current_file, encoding='utf-8') as current_file,
-                open(files.modified_file, encoding='utf-8') as modified_file,
-        ):
-            compare_files(
-                lines1=current_file.readlines(),
-                lines2=modified_file.readlines(),
-                sequence_percent=90
-            )
-
         self.current_file = CurrentFile()
         self.modified_file = ModifiedFile()
 
@@ -62,3 +52,34 @@ class DiffWidget(QWidget):
 
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(splitter)
+
+        with (
+                open(files.current_file, encoding='utf-8') as current_file,
+                open(files.modified_file, encoding='utf-8') as modified_file,
+        ):
+            compare_files(
+                lines1=current_file.readlines(),
+                lines2=modified_file.readlines(),
+                func_equals=self.equals,
+                func_modified=self.modified,
+                func_remove=self.remove,
+                func_added=self.added,
+                sequence_percent=90
+            )
+
+    def equals(self, index1: int, index2: int, text: str):
+        text = text.replace('\n', '')
+        self.current_file.text_edit.append(text)
+        self.modified_file.text_edit.append(text)
+
+    def modified(self, index1: int, index2: int, text1: str, text2: str):
+        self.current_file.text_edit.append(text1.replace('\n', ''))
+        self.modified_file.text_edit.append(text2.replace('\n', ''))
+
+    def remove(self, index: int, text: str):
+        self.current_file.text_edit.append(text.replace('\n', ''))
+        self.modified_file.text_edit.append('')
+
+    def added(self, index: int, text: str):
+        self.current_file.text_edit.append('')
+        self.modified_file.text_edit.append(text.replace('\n', ''))
