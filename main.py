5ff1b4a8b28c3b1bb9ee332fc867d66f8f01d896
volkov.al <volkov.al@tecomgroup.ru>
2024-10-11 13:01:27 +0300

    add readme

--- main.py
+++ main.py
@@ -10,16 +10,15 @@ Config = namedtuple(
     ['current_file', 'modified_file', 'sequence_percent', 'backlight']
 )
 
+config = Config(
+    current_file=r'files/file1.py',
+    modified_file=r'files/file2.py',
+    sequence_percent=1,  # Эксперементальная редактировать не рекомендуется
+    backlight='py'
+)
+
 
 def main():
-
-    config = Config(
-        current_file=r'files/file1.py',
-        modified_file=r'files/file2.py',
-        sequence_percent=1,  # Эксперементальная редактировать не рекомендуется
-        backlight='py'
-    )
-
     app = QApplication([])
     window = widget.Diff(config)
     window.setGeometry(800, 100, 800, 400)
